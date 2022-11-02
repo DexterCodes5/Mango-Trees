fafd410bb450a5df4b024d41fe6b8e9c36bd33ac
dexterlab_5 <103762164+DexterCodes5@users.noreply.github.com>
2022-11-02 15:33:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,66 +12,28 @@ void print(std::vector<std::vector<char>> &arr) {
     std::cout << "\n\n\n";
 }
 
-void drawLines(std::vector<std::vector<char>> &arr, int n, int vertical, int horizontal) {
-    for (int i = 0; i < n; i++) {
-        std::vector<char>::iterator it = arr.at(i).begin();
-        for (int i = 0; i < vertical; i++) {
-            it++;
-        }
-        arr.at(i).insert(it,'|');
-    }
+void drawLines(std::vector<std::vector<char>> &arr, int n, int horizontal, int vertical) {
+    // Horizontal Line
     std::vector<char> v{};
-    for (int i = 0; i <= n; i++) v.push_back('-');
+    for (int i = 0; i < n; i++) v.push_back('-');
     std::vector<std::vector<char>>::iterator it = arr.begin();
     for (int i = 0; i < horizontal; i++) {
         it++;
     }
     arr.insert(it,v);
-}
-
-int countTrees(std::vector<std::vector<char>> &arr, int quadrant, size_t vertical, size_t horizontal) {
-    int trees{};
-    if (quadrant == 1) {
-        for (size_t i = 0; i < vertical; i++) {
-            for (size_t j = 0; j < horizontal; j++) {
-                char curr = arr.at(i).at(j);
-                if (curr == '#') {
-                    trees++;
-                }
-            }
+    
+    // Vertical Line
+    for (int i = 0; i <= n; i++) {
+        std::vector<char>::iterator it = arr.at(i).begin();
+        for (int i = 0; i < vertical; i++) {
+            it++;
         }
-    }
-    else if (quadrant == 2) {
-        for (size_t i = 0; i < vertical; i++) {
-            for (size_t j = horizontal+1; j < arr.at(i).size(); j++) {
-                char curr = arr.at(i).at(j);
-                if (curr == '#') {
-                    trees++;
-                }
-            }
+        if (i == horizontal) {
+            arr.at(i).insert(it,'+');
         }
+        else
+            arr.at(i).insert(it,'|');
     }
-    else if (quadrant == 3) {
-        for (size_t i = vertical+1; i < arr.size(); i++) {
-            for (size_t j = horizontal+1; j < arr.at(i).size(); j++) {
-                char curr = arr.at(i).at(j);
-                if (curr == '#') {
-                    trees++;
-                }
-            }
-        }
-    }
-    else if (quadrant == 4) {
-        for (size_t i = vertical+1; i < arr.size(); i++) {
-            for (size_t j = 0; j < horizontal; j++) {
-                char curr = arr.at(i).at(j);
-                if (curr == '#') {
-                    trees++;
-                }
-            }
-        }
-    }
-    return trees;
 }
 
 int smallestField(int q1, int q2, int q3, int q4) {
@@ -80,26 +42,60 @@ int smallestField(int q1, int q2, int q3, int q4) {
     return v.at(0);
 }
 
-int mangoTrees(std::vector<std::vector<char>> &arr, int n) {
-    std::vector<std::vector<char>> orig = arr;
-    int max_trees{};
+// 0(N^2)
+std::vector<std::vector<int>> auxillaryMatrix(std::vector<std::vector<char>> arr) {
+    std::vector<std::vector<int>> mat(arr.size());
     
-    for (int i = 1; i < n; i++) {
-    
-        for (int j = 1; j < n; j++) {
-        // Draw Lines
-            arr = orig;
-            drawLines(arr,n,i,j);
-            print(arr);
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = 0; j < arr.at(i).size(); j++) { 
+            if (arr.at(i).at(j) == '#')
+                mat.at(i).push_back(1);
+            else
+                mat.at(i).push_back(0);
             
-            // Calc Quadrants
-            int quadrant1 = countTrees(arr,1,j,i);
-            int quadrant2 = countTrees(arr,2,j,i);
-            int quadrant3 = countTrees(arr,3,j,i);
-            int quadrant4 = countTrees(arr,4,j,i);
-            max_trees = std::max(max_trees, smallestField(quadrant1, quadrant2, quadrant3, quadrant1));
         }
     }
+        
+    // Fillin first row and column
+    for (int i = 1; i < arr.size(); i++) {
+        mat.at(0).at(i) += mat.at(0).at(i-1);
+    }
+    for (int i = 1; i < arr.size(); i++) {
+        mat.at(i).at(0) += mat.at(i-1).at(0);
+    }
+    
+    // updating the values of in the cells
+    // as per the general formula
+    for (int i = 1; i < mat.size(); i++) {
+        for (int j = 1; j < mat.at(i).size(); j++) {
+            
+            // values in the cells of new array are updated
+            if (arr.at(i).at(j) == '#')
+                mat.at(i).at(j) = mat.at(i-1).at(j) + mat.at(i).at(j-1) - mat.at(i-1).at(j-1) + 1;
+            else
+                mat.at(i).at(j) = mat.at(i-1).at(j) + mat.at(i).at(j-1) - mat.at(i-1).at(j-1) + 0;
+        }
+    }
+    
+    return mat;
+}
+
+// 0(N^2)
+int mangoTrees(std::vector<std::vector<char>> &arr, int n) {
+    std::vector<std::vector<int>> matrix = auxillaryMatrix(arr);
+    int max_trees{};
+    
+    for (int i = 2; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            // m[x,y] = m[x-1][y] + m[x][j-1] - m[i-1],[j-1] formula 0(1)
+            int quadrant1 = matrix[i][j];
+            int quadrant2 = matrix[i][n-1] - quadrant1;
+            int quadrant3 = matrix[n-1][j] - quadrant1;
+            int quadrant4 = matrix[n-1][n-1] - (quadrant1+quadrant2+quadrant3);
+            max_trees = std::max(max_trees, smallestField(quadrant1, quadrant2, quadrant3, quadrant4));
+        }
+    }
+    
     return max_trees;
 }
 
@@ -112,8 +108,10 @@ int main(){
         {'#','.','.','#','#','.'},
         {'.','#','.','.','.','.'}
     };
-    std::cout << mangoTrees(arr,6);
-    //print(arr);
+    std::cout << mangoTrees(arr,6) << "\n";
+    // propper field
+    drawLines(arr,6,3,2);
+    print(arr);
     
     return 0;
 }
